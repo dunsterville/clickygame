d01bca9ce5acda759ba54e53b99da6aee00c097d
Bert Keller <bertwinkeller@gmail.com>
2020-01-15 20:21:31 -0800

    images

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
 import Jumbotron1 from './components/Jumbotron1'
 import GameBoard from './components/GameBoard'
-import images from '../../Images/index.js'
-
+import friendsData from './data/friendsData.json'
 
 class App extends Component {
   
+state = {
+  friendsData : friendsData,
+  score: '',
+  topScore: '',
   
+}
+  
+  
+
   render () {
     return(
     <div>
     <Jumbotron1/>
-    <GameBoard>
+    <GameBoard friendsData = {this.state.friendsData}
+    />
     
-    </GameBoard>
   </div>
     )
   }
--- clickygame/src/components/GameBoard/GameBoard.js
+++ clickygame/src/components/GameBoard/GameBoard.js
@@ -1,52 +1,27 @@
 import React from 'react'
-import {Row, Container, Col} from 'react-bootstrap'
-import ImageCard from '../../components/ImageCard'
+import { Row, Container, Col } from 'react-bootstrap'
+// import ImageCard from '../../components/ImageCard'
+import { Card } from 'react-bootstrap'
 
 const GameBoard = (props) => {
-    console.log
+  let teamCards = props.friendsData.map(team => {
+    return (
+      <Col sm="4">
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <img height="200px" width="auto" position="center" src={team.image} />
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  })
   return (
-<Container>
-
-<Row>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-  </Row>
-
-
-
-  <Row>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-  </Row>
-
-
-  <Row>
-    <Col sm>
-        <ImageCard/>    
-    </Col>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-    <Col sm>
-        <ImageCard/>
-    </Col>
-  </Row>
-
-</Container>
+    <Container>
+      <Row>
+        {teamCards}
+        <br/>
+      </Row>
+    </Container>
   )
 }
 
