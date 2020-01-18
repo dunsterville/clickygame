cd2d216489479b406b39270f7bda7261e8b1dd23
Bert Keller <bertwinkeller@gmail.com>
2020-01-18 13:57:20 -0800

    added production code

---
+++ clickygame/build/precache-manifest.f5c58938c3184c72079db65a1c9b5baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bef95dab4526d32cb47ce195bdffb3d",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "8f0d28f1663aee45fb70",
+    "url": "/clickygame/static/js/2.f3ac2b9e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clickygame/static/js/2.f3ac2b9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e355e3e56d686e1fdfdb",
+    "url": "/clickygame/static/js/main.272d474e.chunk.js"
+  },
+  {
+    "revision": "bf1213cf7b9f7cbe58bc",
+    "url": "/clickygame/static/js/runtime-main.c4199c52.js"
+  }
+]);
